ca127b13b52c8991cc9d5bcfde9fa8b948eababe
ManulThisuraka <it20163754@my.sliit.lk>
2022-02-22 14:20:46 +0530

    first commit

---
+++ components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Header = ({title}) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Shopping List',
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: 'darkslateblue',
+  },
+  text: {
+    color: 'white',
+    fontSize: 23,
+    textAlign: 'center',
+  },
+
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 75 / 2,
+  },
+});
+
+export default Header;
